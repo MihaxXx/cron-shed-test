c3f74729ea1a58039a66ab974f9bd1b3bd70a97f
Mikhail <mikes22@yandex.ru>
2023-09-17 03:17:07 +0300

    Add days of month

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import {useState} from "react";
 
 function App() {
-  const [formData, setFormData] = useState({selectedPeriod: "year", selectedMonth: [""], MonthOption: ""});
+  const [formData, setFormData] = useState({
+    selectedPeriod: "year",
+    selectedMonth: [""],
+    monthOption: "on",
+    daysOfMonth: [""],
+    everyNthDay: ""
+  });
   const handleChange = (event) => {
     const {name, value} = event.target;
     setFormData((prevFormData) => ({...prevFormData, [name]: value}));
-    console.log(value)
+    console.log(name + ': ' + value)
   };
 
   const handleMultipleChange = (event) => {
     const name = event.target.name;
     const value = Array.from(event.target.selectedOptions, option => option.value)
     setFormData((prevFormData) => ({...prevFormData, [name]: value}));
-    console.log(value)
+    console.log(name + ': ' + value)
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`Period: ${formData.selectedPeriod}, Month: ${formData.selectedMonth}, Message: ${formData.message}`
+    alert(Object.keys(formData).map(function (k) {
+        return k + ":" + formData[k]
+      }).join("\n")
     );
   };
 
@@ -42,34 +50,79 @@ function App() {
             </div>
             {formData.selectedPeriod === "year" &&
               <div className="form-group row">
-              <label className="col-form-label">in</label>
-              <div className="col-sm-4">
-                <select className="selectpicker form-control" multiple={true} name="selectedMonth" value={formData.selectedMonth}
-                        onChange={handleMultipleChange}>
-                  <option value="1">January</option>
-                  <option value="2">February</option>
-                  <option value="3">March</option>
-                  <option value="4">April</option>
-                  <option value="5">May</option>
-                  <option value="6">June</option>
-                  <option value="7">July</option>
-                  <option value="8">August</option>
-                  <option value="9">September</option>
-                  <option value="10">October</option>
-                  <option value="11">November</option>
-                  <option value="12">December</option>
-                </select>
-              </div>
-            </div>}
+                <label className="col-form-label">in</label>
+                <div className="col-sm-4">
+                  <select className="selectpicker form-control" multiple={true} name="selectedMonth"
+                          value={formData.selectedMonth}
+                          onChange={handleMultipleChange}>
+                    <option value="1">January</option>
+                    <option value="2">February</option>
+                    <option value="3">March</option>
+                    <option value="4">April</option>
+                    <option value="5">May</option>
+                    <option value="6">June</option>
+                    <option value="7">July</option>
+                    <option value="8">August</option>
+                    <option value="9">September</option>
+                    <option value="10">October</option>
+                    <option value="11">November</option>
+                    <option value="12">December</option>
+                  </select>
+                </div>
+              </div>}
             {(formData.selectedPeriod === "year" || formData.selectedPeriod === "month") &&
               <div className="form-group row">
-                <div className="col-sm-2">
-                  <select className="form-control" name="MonthOption" value={formData.MonthOption}
+                <div className="col-sm-2-ml-0">
+                  <select className="form-control" name="monthOption" value={formData.monthOption}
                           onChange={handleChange}>
                     <option value="on">on</option>
                     <option value="every">every</option>
                   </select>
                 </div>
+                <div className="col-sm-4" style={{display: formData.monthOption === "on" ? 'block' : 'none'}}>
+                  <select className="selectpicker form-control" name="daysOfMonth" value={formData.daysOfMonth}
+                          multiple={true} onChange={handleMultipleChange}>
+                    <option value="1">01</option>
+                    <option value="2">02</option>
+                    <option value="3">03</option>
+                    <option value="4">04</option>
+                    <option value="5">05</option>
+                    <option value="6">06</option>
+                    <option value="7">07</option>
+                    <option value="8">08</option>
+                    <option value="9">09</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                    <option value="21">21</option>
+                    <option value="22">22</option>
+                    <option value="23">23</option>
+                    <option value="24">24</option>
+                    <option value="25">25</option>
+                    <option value="26">26</option>
+                    <option value="27">27</option>
+                    <option value="28">28</option>
+                    <option value="29">29</option>
+                    <option value="30">30</option>
+                    <option value="31">31</option>
+                  </select>
+                </div>
+                <div style={{display: formData.monthOption === "on" ? 'none' : 'block'}}
+                     className="form-outline col-sm-2 ml-2">
+                  {/*TODO: Set min max val*/}
+                  <input type="number" className="form-control" name="everyNthDay" value={formData.everyNthDay}
+                         onChange={handleChange}/>
+                </div>
+                <label style={{display: formData.monthOption === "on" ? 'none' : 'block'}}
+                       className="col-form-label">days</label>
               </div>}
 
             <div className="form-group">
